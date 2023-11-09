85f46e343cf93a4016db7f11ca37f41ed76e0009
Aidan Gollan <aidangollan@icloud.com>
2023-11-08 19:04:47 -0500

    pretty!

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from flask import Flask, request, jsonify, send_from_directory, redirect, session, url_for
-from search import search_algo, search_for_song
+from flask import Flask, request, jsonify, send_from_directory, redirect, session
+from search import search_algo
 from auth import get_token
-from models import Song
 from db import db
 import requests
 import os
 from flask_cors import CORS
 import base64
-import time
+from create_playlist import create_playlist_on_spotify
 
 #from dotenv import load_dotenv
 #load_dotenv()
-
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app, supports_credentials=True)
@@ -28,20 +26,12 @@ def login():
     is_guest = request.args.get('guest') == 'true'
     
     if is_guest:
-        # Handle the guest login logic here, for example:
-        # Set the session['user_type'] = 'guest'
-        print("guest login")
         session['user_type'] = 'guest'
         user_token = refresh_access_token(os.getenv('GUEST_REFRESH_TOKEN'))
         session['token'] = user_token
-        print("Access Token:", session['token'])
-        print("'scope:', session['scope']")
         return redirect("/search")
 
-    
-    # If not a guest, proceed with Spotify login
     session['user_type'] = 'normal'
-    print("normal login")
 
     auth_query_parameters = {
         "response_type": "code",
@@ -85,9 +75,7 @@ def callback():
     post_request = requests.post(os.getenv("TOKEN_URL"), data=code_payload)
     response_data = post_request.json()
     session['token'] = response_data["access_token"]
-    
-    print("Access Token:", session['token'])
-    
+
     return redirect("/search")
 
 @app.route('/api/logout')
@@ -98,106 +86,30 @@ def logout():
 
 @app.route("/api/search", methods=["POST"])
 def search():
-    print("in search")
-    token = get_token() #my token, not user, used for searching for songs
+    token = get_token()
     query = request.json.get("query").strip(" ")
-    print(query)
     result = search_algo(token, query)
-    print(result)
     
     return result
 
 @app.route("/search")
 def search_default():
-    print("in search default")
     if session.get('user_type') is None:
-        print("no user type")
         return redirect("/")
-    print("user exists")
-    print(session.get('user_type'))
     return send_from_directory(app.static_folder, 'index.html')
 
 @app.route("/api/create_playlist", methods=["POST"])
 def create_playlist():
-    print("in create playlist")
     user_token = session.get('token')
-
-    print(user_token)
     
     song_ids = request.json.get("song_ids")
-    print(song_ids)
-    print("song ids")
 
     response_data = create_playlist_on_spotify(user_token, song_ids)
 
-    print(response_data)
-    
     if response_data["success"]:
         return jsonify(response_data)
     else:
         return jsonify(response_data), 500
-    
-def create_playlist_on_spotify(user_token, song_ids):
-    print("in create playlist on spotify")
-    print("song_ids")
-    print(song_ids)
-    SPOTIFY_API_BASE_URL = "https://api.spotify.com/v1"
-    HEADERS = {
-        "Authorization": f"Bearer {user_token}"
-    }
-
-    # 1. Get the current user's ID
-    current_user_request = requests.get(f"{SPOTIFY_API_BASE_URL}/me", headers=HEADERS)
-    current_user_data = current_user_request.json()
-    user_id = current_user_data['id']
-
-    print(user_id)
-    print("user id")
-
-    # 2. Create a new playlist for the user
-    create_playlist_data = {
-        "name": "My Custom Playlist",
-        "description": "Created using Weird Spotify",
-        "public": True
-    }
-    create_playlist_request = requests.post(f"{SPOTIFY_API_BASE_URL}/users/{user_id}/playlists", headers=HEADERS, json=create_playlist_data)
-    new_playlist_data = create_playlist_request.json()
-    playlist_id = new_playlist_data['id']
-
-    print(playlist_id)
-    print("playlist id")
-
-    # 3. Add tracks to the new playlist
-    add_tracks_data = {
-        "uris": [f"spotify:track:{song_id}" for song_id in song_ids]
-    }
-    add_tracks_request = requests.post(f"{SPOTIFY_API_BASE_URL}/playlists/{playlist_id}/tracks", headers=HEADERS, json=add_tracks_data)
-    add_tracks_response = add_tracks_request.json()
-
-    print(add_tracks_response)
-    print("add tracks response")
-
-    print(add_tracks_request.status_code)
-    print("add tracks request status code")
-
-    time.sleep(.5)
-
-    if add_tracks_request.status_code == 201:
-        print("success")
-        print(new_playlist_data['external_urls']['spotify'])
-        return {
-            "success": True,
-            "message": "Playlist created successfully",
-            "playlist_url": new_playlist_data['external_urls']['spotify']
-        }
-    else:
-        print("failed")
-        print(add_tracks_response)
-        return {
-            "success": False,
-            "message": "Failed to add tracks to the playlist",
-            "playlist_url": None
-        }
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -11,16 +11,16 @@ function LoginPage() {
     return (
         <div className="spotify-login-page">
             <h1>Welcome to Weird Spotify!</h1>
-            <p>This is a work-in-progress project where I aim to create a tool capable of generating a Spotify playlist. 
-                The names of the songs in the playlist will match the sentence you enter.</p>
-            <p>Currently you need prior approval to login and add playlists to your account.
-                Others will have to use guest mode.</p>
-            <p>When using the app as a guest, the create playlist account 
-                will create the playlist on a dummy account.</p>
-            <button className="spotify-button" onClick={() => handleLoginClick(false)}>Login with Spotify (Approval Needed!)</button>
-            <button className="spotify-button guest-button" onClick={() => handleLoginClick(true)}>Continue as Guest</button>
+            <p>This project allows you to do what you've always wanted to but never could</p>
+            <br />
+            <p>Turn a sentence into a Spotify Playlist!!</p>
+            <br />
+            <p>Enter any sentence, in any language, then watch the magic happen!</p>
+            <button className="spotify-button guest-button" onClick={() => handleLoginClick(true)}>Lets Go!</button>
         </div>
     );
 }
 
 export default LoginPage;
+
+//<button className="spotify-button" onClick={() => handleLoginClick(false)}>Login with Spotify (Approval Needed!)</button>
