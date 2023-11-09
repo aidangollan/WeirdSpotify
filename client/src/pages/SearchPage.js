288648f3c40c58bedaa6d0b513cbe87fde588a67
Aidan Gollan <aidangollan@icloud.com>
2023-11-08 20:37:53 -0500

    logo spinny wheee

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { handleFormSubmit } from '../api';
+import SpotifyLogo9 from '../spotify-9.png';
 import '../styles.css';
 
 import NavBar from '../components/NavBar';
@@ -10,17 +11,25 @@ function SearchPage() {
     const [songs, setSongs] = useState([]);
     const [errors, setErrors] = useState([]);
     const [query, setQuery] = useState('');
+    const [animationState, setAnimationState] = useState('off-screen');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const result = await handleFormSubmit(query);
+        setAnimationState('entering-spinning'); // Start entering and spinning animation
         
+        // Perform the search and wait for the result.
+        const result = await handleFormSubmit(query);
+    
+        // Set the animation state to 'exiting' once the search is complete.
         if (Array.isArray(result.songs)) {
             setSongs(result.songs);
+            setAnimationState('exiting'); // Trigger the exit animation
+            // Set a timeout to change the state to 'idle' after the exit animation duration.
+            setTimeout(() => setAnimationState('idle'), 1000); // Match with the duration of the exit animation
         } else {
             console.error("Received unexpected songs data format from server");
         }
-
+    
         if (Array.isArray(result.errors)) {
             setErrors(result.errors);
         } else {
@@ -49,6 +58,17 @@ function SearchPage() {
                 />
                 <ResultsList songs={modifiedSongs} errors={errors} />
             </div>
+            <img 
+                src={SpotifyLogo9}
+                className={`spotify-logo ${
+                    animationState === 'entering-spinning'
+                        ? 'logo-entering-spinning'
+                        : animationState === 'exiting'
+                        ? 'logo-exiting'
+                        : 'logo-idle'
+                }`} 
+                alt="Spotify Logo"
+            />
         </div>
     );
 }
